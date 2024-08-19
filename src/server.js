e670e8a2d786a093cf923f53f7983c6b828eeee3
Oksana Ivantsiv <59508997+Olja2020@users.noreply.github.com>
2024-08-19 17:55:48 +0300

    add

--- src/server.js
+++ src/server.js
@@ -1,7 +1,7 @@
 import express from 'express';
 import cookieParser from 'cookie-parser';
 import contactRouter from './routers/contacts.js';
-import authRoutes from './routers/auth.js';
+//import authRoutes from './routers/auth.js';
 import { env } from './utils/env.js';
 import { swaggerDocs } from './middlewares/swaggerDocs.js';
 
@@ -15,17 +15,15 @@ export const setupServer = () => {
   const app = express();
 
   app.use(cookieParser());
- app.use(contactRouter);
-   app.use('/uploads', express.static(UPLOAD_DIR));
+
+  app.use('/uploads', express.static(UPLOAD_DIR));
   app.use('/api-docs', swaggerDocs());
   //app.use(authRoutes);
- 
-
+  app.use(contactRouter);
   app.use('*', notFoundHandler);
 
   app.use(errorHandler);
 
- 
   app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
   });
