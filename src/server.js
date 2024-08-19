2616ebb90a9e299548fd0145839c2a2407c3906f
Oksana Ivantsiv <59508997+Olja2020@users.noreply.github.com>
2024-08-19 18:55:55 +0300

    add

--- src/server.js
+++ src/server.js
@@ -15,14 +15,14 @@ export const setupServer = () => {
   const app = express();
 
   app.use(cookieParser());
-
+  app.use(contactRouter);
   app.use('/uploads', express.static(UPLOAD_DIR));
   app.use('/api-docs', swaggerDocs());
   //app.use(authRoutes);
-  app.use(contactRouter);
-  app.use('*', notFoundHandler);
 
   app.use(errorHandler);
+
+  app.use('*', notFoundHandler); //notFoundHandler
 
   app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
